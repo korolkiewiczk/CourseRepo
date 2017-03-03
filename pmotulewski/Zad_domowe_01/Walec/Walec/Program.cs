21ee0fb87980f61116f2fe925f76f2f46b7601d5
Pawel Motulewski <pawel.motulewski@pragmasoft.pl>
2017-03-03 19:06:18 +0100

    pmotulewski Walec

--- pmotulewski/Zad_domowe_01/Walec/Walec/Program.cs
+++ pmotulewski/Zad_domowe_01/Walec/Walec/Program.cs
@@ -10,20 +10,21 @@ namespace Walec
     {
         static void Main(string[] args)
         {
-            int pipe_height, ammount_of_disc=0;
+            int pipe_height=0, ammount_of_disc=0;
             int[] pipe;
             int[] discs;
             bool is_Correct = false;
 
             do
             {
-                char space = ' ';
                 Console.WriteLine("Podaj Wysokośc rurki i liczbę dysków kóra zamierzasz wrzucić oddzielone spacją");
                 string height_and_discs = Console.ReadLine();
 
-                if (height_and_discs.Split(space).Length == 2)
+                var splitNumbers = height_and_discs.Split(' ');
+                splitNumbers.mamNadziejeZeTakaFunkcjaIstnieje = "";
+                if (splitNumbers.Length == 2)
                 {
-                    if (Int32.TryParse(height_and_discs.Split(space)[0], out pipe_height) & Int32.TryParse(height_and_discs.Split(space)[1], out ammount_of_disc))
+                    if (Int32.TryParse(splitNumbers[0], out pipe_height) && Int32.TryParse(splitNumbers[1], out ammount_of_disc))
                     {
                         is_Correct = true;
                                            }
@@ -34,12 +35,19 @@ namespace Walec
 
             do
             {
-                char space = ' ';
+                int radius;
                 Console.WriteLine("Podaj promienie walców oddzielone spacjami. Walców ma być " + ammount_of_disc.ToString());
+                string pipe_radius_string = Console.ReadLine();
+                string[] pipe_radius = pipe_radius_string.Split(' ');
+                pipe = Array.ConvertAll(pipe_radius, int.Parse); 
+
 
             } while (is_Correct == false);
 
-
+            Console.WriteLine("");
+            Console.WriteLine(" Rurka ma wyskość: " + pipe_height);
+            var joinedArray = String.Join(", ", pipe);
+            Console.WriteLine($"Wymiary rurki to: {joinedArray} " );
 
         }
     }
